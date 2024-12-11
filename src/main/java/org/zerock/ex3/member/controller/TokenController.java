ca0849f8ac156da570af37d9487c9e84a1a3017e
dengo23-dev <dengo23.dev@gmail.com>
2024-12-11 16:31:22 +0900

    AccessToken생성

--- src/main/java/org/zerock/ex3/member/controller/TokenController.java
+++ src/main/java/org/zerock/ex3/member/controller/TokenController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.ex3.member.dto.MemberDTO;
+import org.zerock.ex3.member.security.util.JWTUtil;
 import org.zerock.ex3.member.service.MemberService;
 
 import java.util.Map;
@@ -21,6 +22,8 @@ public class TokenController {
   
   private final MemberService memberService;
   
+  private final JWTUtil jwtUtil;
+  
   @PostMapping("/make")
   public ResponseEntity<Map<String, String>> makeToken(@RequestBody MemberDTO memberDTO) {
     log.info("make token..............................");
@@ -28,6 +31,21 @@ public class TokenController {
     MemberDTO memberDTOResult = memberService.read(memberDTO.getMid(), memberDTO.getMpw());
     log.info(memberDTOResult);
     
-    return null;
+    
+    //180p추가
+    String mid = memberDTOResult.getMid();
+    Map<String, Object> dataMap = memberDTOResult.getDataMap();
+    
+    String accessToken = jwtUtil.createToken(dataMap, 10);
+    
+    String refreshToken = jwtUtil.createToken(Map.of("mid", mid), 60 * 24 * 7);
+    
+    log.info("accessToken: " + accessToken);
+    log.info(("refreshTOken: " + refreshToken));
+    
+    
+    return ResponseEntity.ok(
+        Map.of("accessToken", accessToken, "refreshToken", refreshToken)
+    );
   }
 }
--- src/main/java/org/zerock/ex3/member/exception/MemberExceptions.java
+++ src/main/java/org/zerock/ex3/member/exception/MemberExceptions.java
@@ -18,5 +18,4 @@ public enum MemberExceptions {
   public MemberTaskException get(){
     return memberTaskException;
   }
-  
 }
